5467766a824c8ef3f2ee3e29c1663023db0b2cec
Anna Kuprym <annakupriyanchik99@gmail.com>
2024-01-26 23:25:51 +0300

    Updated previous commit >> Created movable Card via storyboard.

--- Tinder-like-SwipeApp/ViewController.swift
+++ Tinder-like-SwipeApp/ViewController.swift
@@ -9,21 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
     @IBOutlet weak var card: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view = UIView()
         view.addSubview(card)
-        view.backgroundColor = .blue
     }
     
     @IBAction func panCard(_ sender: UIPanGestureRecognizer) {
         let card = sender.view!
-        // how far you moved the cursor
         let point = sender.translation(in: view)
         card.center = CGPoint(x: view.center.x + point.x, y: view.center.y + point.y)
     }
+    
     
 }
