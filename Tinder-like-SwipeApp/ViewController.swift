501c647efd0715eeaccf63cdffabfab8b7c79558
Anna Kuprym <annakupriyanchik99@gmail.com>
2024-01-29 19:54:49 +0300

    Added thumbUp/Down, Changed Thumb.alpha Added Animation to get card back to Center.

--- Tinder-like-SwipeApp/ViewController.swift
+++ Tinder-like-SwipeApp/ViewController.swift
@@ -11,16 +11,37 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var card: UIView!
     
+    @IBOutlet weak var thumbImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view = UIView()
+        view.backgroundColor = .gray
         view.addSubview(card)
     }
     
     @IBAction func panCard(_ sender: UIPanGestureRecognizer) {
         let card = sender.view!
         let point = sender.translation(in: view)
+        let xFromCenter = card.center.x - view.center.x
+        
         card.center = CGPoint(x: view.center.x + point.x, y: view.center.y + point.y)
+        
+        if xFromCenter > 0 {
+            thumbImageView.image = UIImage(systemName: "hand.thumbsup.fill")
+            thumbImageView.tintColor = .green
+        } else {
+            thumbImageView.image = UIImage(systemName: "hand.thumbsdown.fill")
+            thumbImageView.tintColor = .red
+        }
+        
+        thumbImageView.alpha = abs(xFromCenter / view.center.x)
+        
+        if sender.state == UIGestureRecognizer.State.ended {
+            UIView.animate(withDuration: 0.2) {
+                card.center = self.view.center
+            }
+        }
     }
     
     
