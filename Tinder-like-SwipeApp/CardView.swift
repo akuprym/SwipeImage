6eb3c1307866bbde4db1509538dab1518e8dbc79
Anna Kuprym <annakupriyanchik99@gmail.com>
2024-04-12 23:32:27 +0300

    Updated VC

---
+++ Tinder-like-SwipeApp/CardView.swift
@@ -0,0 +1,20 @@
+//
+//  CardView.swift
+//  Tinder-like-SwipeApp
+//
+//  Created by admin on 12.04.24.
+//
+
+import UIKit
+
+class CardView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Tinder-like-SwipeApp/CardViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  CardViewModel.swift
+//  Tinder-like-SwipeApp
+//
+//  Created by admin on 12.04.24.
+//
+
+import Foundation
