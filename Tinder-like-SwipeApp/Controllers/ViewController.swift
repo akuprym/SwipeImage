5e1d3b34dc87ffd5dfe58eaf29e5c2aa2edfd32f
Anna Kuprym <annakupriyanchik99@gmail.com>
2024-04-16 21:20:17 +0300

    Upgrade VC

--- Tinder-like-SwipeApp/Controllers/ViewController.swift
+++ Tinder-like-SwipeApp/Controllers/ViewController.swift
@@ -12,12 +12,11 @@ class ViewController: UIViewController {
     var currentPage = 1
     var currentPhotoIndex = 0
     let treshold: CGFloat = 75
-    var isLoading = false
+    let photoIndexLimit = 9
     
     var cardView: UIView!
     var imageView: UIImageView!
     var thumbImageView: UIImageView!
-    var likeButton: UIBarButtonItem!
     
     var results: [APIResponse] = []
     var images: [UIImage] = []
@@ -25,7 +24,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
-        
+        setupUI()
+   
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan))
+        cardView.addGestureRecognizer(panGesture)
+
+        fetchPhotos()
+    }
+    
+    // MARK: - Setup UI
+    
+    fileprivate func setupUI() {
         cardView = UIView(frame: CGRect(x: 0, y: 0, width: 380, height: 500))
         cardView.layer.cornerRadius = 10
         cardView.layer.shadowColor = UIColor.black.cgColor
@@ -47,16 +56,11 @@ class ViewController: UIViewController {
         imageView.contentMode = .scaleAspectFill
         imageView.clipsToBounds = true
         cardView.addSubview(imageView)
-        loadNextSetOfPhotos()
-        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan))
-        cardView.addGestureRecognizer(panGesture)
+        
         thumbImageView = UIImageView()
         cardView.addSubview(thumbImageView)
         thumbImageView.frame = imageView.bounds
-        
-        loadNextSetOfPhotos()
     }
-
     // MARK: - Swiping Gestures
     
     @objc fileprivate func handlePan(gesture: UIPanGestureRecognizer) {
@@ -108,33 +112,28 @@ class ViewController: UIViewController {
     
     fileprivate func swipedLeft() {
         // Dislike
-        if currentPhotoIndex < 9 {
-            currentPhotoIndex += 1
-            imageView.image = images[currentPhotoIndex]
+        if currentPhotoIndex < photoIndexLimit {
             UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0.1) {
                 self.cardView.center = CGPoint(x: self.cardView.frame.origin.x - self.cardView.frame.width/2, y: self.view.center.y + self.treshold)
                 self.thumbImageView.image = UIImage(systemName: "hand.thumbsup.fill")
                 self.thumbImageView.tintColor = .green
-                self.resetImageViewPosition()
             }
+            resetCard()
         } else {
-            loadNextSetOfPhotos()
+            fetchPhotos()
             currentPhotoIndex = 0
         }
     }
     
     fileprivate func swipedRight() {
         // Like
-        if currentPhotoIndex < 9 {
-            imageView.image = images[currentPhotoIndex]
+        if currentPhotoIndex < photoIndexLimit {
             UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0.1) {
                 self.cardView.center = CGPoint(x: self.view.frame.maxX + self.view.frame.width/2, y: self.imageView.center.y + self.treshold)
-                self.images.append(self.imageView.image!)
-                self.currentPhotoIndex += 1
-                self.resetImageViewPosition()
             }
+            resetCard()
         }  else {
-            loadNextSetOfPhotos()
+            fetchPhotos()
             currentPhotoIndex = 0
         }
     }
@@ -148,95 +147,71 @@ class ViewController: UIViewController {
         } else if cardView.center.x > view.frame.width - treshold {
             swipedRight()
         } else {
-            resetImageViewPosition()
+            cardView.transform = .identity
+            cardView.center = self.view.center
+            thumbImageView.alpha = 0
         }
     }
     
-    func resetImageViewPosition() {
-         UIView.animate(withDuration: 0.3) {
-             self.imageView.center = self.view.center
-         }
-     }
-    
-    
-//    fileprivate func resetCard() {
-//        UIView.animate(withDuration: 0.5) {
-//            self.currentPhotoIndex += 1
-//            self.cardView.transform = .identity
-//            self.cardView.center = self.view.center
-////            self.downloadImage()
-//        }
-//    }
+    fileprivate func resetCard() {
+        UIView.animate(withDuration: 0.5) {
+            self.currentPhotoIndex += 1
+            self.cardView.transform = .identity
+            self.cardView.center = self.view.center
+            self.thumbImageView.alpha = 0
+            self.downloadImage()
+        }
+    }
     
     // MARK: - Network Service
     
-    fileprivate func loadNextSetOfPhotos() {
-        
-        guard !isLoading else { return }
-                isLoading = true
+    fileprivate func fetchPhotos() {
         
         let unsplashAPIKey = "FrxT9u6XQRE_HVqjS9MhfYTH5LN0SsnhIp8VheooyRs"
         let urlString = "https://api.unsplash.com/photos/random?client_id=\(unsplashAPIKey)&count=10&page=\(currentPage)"
-
+        
         guard let url = URL(string: urlString) else { return }
-               
-               let task = URLSession.shared.dataTask(with: url) { [weak self] (data, _, error) in
-                   guard let self = self else { return }
-                   defer { self.isLoading = false }
-                   
-                   if let error = error {
-                       print("Error fetching images: \(error)")
-                       return
-                   }
-                   
-                   guard let data = data else { return }
-                   
+        
+        let task = URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data = data else { return }
+            
             do {
                 let jsonResult = try JSONDecoder().decode([APIResponse].self, from: data)
-                let imageUrls = jsonResult.compactMap { URL(string: $0.urls.regular) }
-                               self.images = imageUrls.compactMap { try? Data(contentsOf: $0) }.compactMap { UIImage(data: $0) }
-                               
-                               DispatchQueue.main.async {
-                                   self.imageView.image = self.images.first
-                               }
-//                DispatchQueue.main.async {
-//                    self.results = jsonResult
-//                    self.downloadImage()
-//                                   
-//                }
+                DispatchQueue.main.async {
+                    self.results = jsonResult
+                    self.downloadImage()
+                }
             } catch {
                 print(error)
             }
         }
         task.resume()
     }
-
     
-//    fileprivate func downloadImage() {
-//        
-//        if currentPhotoIndex > 9 {
-//            currentPage += 1
-//            currentPhotoIndex = 0
-//            fetchPhotos()
-//        } else {
-//            thumbImageView.alpha = 0
-//            let urlString = self.results[currentPhotoIndex].urls.regular
-//            guard let url = URL(string: urlString) else { return }
-//            let request = URLRequest(url: url, cachePolicy: .returnCacheDataElseLoad, timeoutInterval: 5)
-//            let dataTask = URLSession.shared.dataTask(with: request) { [weak self] data, response, error in
-//                guard error == nil,
-//                      let data = data,
-//                      let response = response as? HTTPURLResponse,
-//                      response.statusCode == 200 else { return }
-//                guard let image = UIImage(data: data) else { return }
-//                DispatchQueue.main.async {
-//                    self?.imageView.image = image
-//                    self?.images = image.compactMap { try? Data(contentsOf: $0) }.compactMap { UIImage(data: $0) }
-//                }
-//            }
-//            dataTask.resume()
-//        }
-//    }
+    fileprivate func downloadImage() {
+        
+        if currentPhotoIndex > photoIndexLimit {
+            currentPage += 1
+            currentPhotoIndex = 0
+            fetchPhotos()
+        } else {
+            thumbImageView.alpha = 0
+            let urlString = self.results[currentPhotoIndex].urls.regular
+            guard let url = URL(string: urlString) else { return }
+            let request = URLRequest(url: url, cachePolicy: .returnCacheDataElseLoad, timeoutInterval: 5)
+            let dataTask = URLSession.shared.dataTask(with: request) { [weak self] data, response, error in
+                guard error == nil,
+                      let data = data,
+                      let response = response as? HTTPURLResponse,
+                      response.statusCode == 200 else { return }
+                guard let image = UIImage(data: data) else { return }
+                DispatchQueue.main.async {
+                    self?.imageView.image = image
+                }
+            }
+            dataTask.resume()
+        }
+    }
 
     
 }
